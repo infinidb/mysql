5857185c4aa272db7fd5adfe03d0bf7a29bdcd83
Bob Dempsey <rdempsey@calpont.com>
2013-10-11 12:51:43 -0500

    handle other libtools

--- idbbuild.sh
+++ idbbuild.sh
@@ -15,22 +15,9 @@ done
 
 extra_ld_flags="-Wl,-rpath -Wl,$prefix/mysql/lib/mysql -Wl,-rpath -Wl,$prefix/lib"
 
-autoreconf
 ./configure --prefix=$prefix/mysql $WITH_DEBUG --without-libedit --with-readline \
         --with-plugins=csv,heap,myisam,myisammrg,partition --with-mysqld-ldflags="$extra_ld_flags" \
         --with-client-ldflags="$extra_ld_flags" --with-extra-charsets=all --with-ssl
-
-# Hmm... libtool script is jacked on newer CentOS?
-grep -Eqs 'CentOS release 6' /etc/redhat-release
-rc1=$?
-lsb_release -a 2>/dev/null | grep -Eqs squeeze
-rc2=$?
-if [ $rc1 -eq 0 -o $rc2 -eq 0 ]; then
-	grep -Eqs 'VERSION=1.5.22' libtool
-	if [ $? -eq 0 ]; then
-		/bin/cp -f /usr/bin/libtool .
-	fi
-fi
 
 make
 
