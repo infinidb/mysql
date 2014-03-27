83c6f078555e7eccff6e33a22cb07a8640749866
Zhixuan Zhu <zzhu@calpont.com>
2014-03-27 11:54:32 -0500

    Bug 5910 - Object names > 64 cause replication errors
    Fixed mysql's inconsistent cast for maximum db_len 128.

--- sql/log_event.cc
+++ sql/log_event.cc
@@ -2752,7 +2752,8 @@ Query_log_event::Query_log_event(const char* buf, uint event_len,
   
   slave_proxy_id= thread_id = uint4korr(buf + Q_THREAD_ID_OFFSET);
   exec_time = uint4korr(buf + Q_EXEC_TIME_OFFSET);
-  db_len = (uint)buf[Q_DB_LEN_OFFSET]; // TODO: add a check of all *_len vars
+  // @InfiniDB @bug5910 fix for db_len=128
+  db_len = (uchar)buf[Q_DB_LEN_OFFSET]; // TODO: add a check of all *_len vars
   error_code = uint2korr(buf + Q_ERR_CODE_OFFSET);
 
   /*
