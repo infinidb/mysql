feb425b5ebeaa212c0c753820e471734e991b18f
Zhixuan Zhu <zzhu@calpont.com>
2014-06-20 13:49:13 -0500

    rollback to commit 66aa84f4baff1d5866cc4568dd36379b037385fe

--- idbbuild.sh
+++ idbbuild.sh
@@ -4,13 +4,13 @@ prefix=/usr/local/Calpont
 WITH_DEBUG=
 
 for arg in "$@"; do
-if [ `expr -- "$arg" : '--prefix='` -eq 9 ]; then
-prefix="`echo $arg | awk -F= '{print $2}'`"
-elif [ `expr -- "$arg" : '--with-debug'` -eq 12 ]; then
-WITH_DEBUG=--with-debug
-else
-echo "ignoring unknown argument: $arg" 1>&2
-fi
+	if [ `expr -- "$arg" : '--prefix='` -eq 9 ]; then
+		prefix="`echo $arg | awk -F= '{print $2}'`"
+	elif [ `expr -- "$arg" : '--with-debug'` -eq 12 ]; then
+		WITH_DEBUG=--with-debug
+	else
+		echo "ignoring unknown argument: $arg" 1>&2
+	fi
 done
 
 extra_ld_flags="-Wl,-rpath -Wl,$prefix/mysql/lib/mysql -Wl,-rpath -Wl,$prefix/lib"
@@ -20,5 +20,4 @@ extra_ld_flags="-Wl,-rpath -Wl,$prefix/mysql/lib/mysql -Wl,-rpath -Wl,$prefix/li
         --with-client-ldflags="$extra_ld_flags" --with-extra-charsets=all --with-ssl
 
 make
-
 
