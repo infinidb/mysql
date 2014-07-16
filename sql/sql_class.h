b758bc2c81944b01774262c8850834292515deb6
Cindy Hao <chao@calpont.com>
2014-07-16 10:08:28 -0500

    Bug 6121 - DELETE with USING clause deletes data from the wrong table
    make number of tables in delete piblic

--- sql/sql_class.h
+++ sql/sql_class.h
@@ -3083,7 +3083,6 @@ class multi_delete :public select_result_interceptor
   TABLE_LIST *delete_tables, *table_being_deleted;
   Unique **tempfiles;
   ha_rows deleted, found;
-  uint num_of_tables;
   int error;
   bool do_delete;
   /* True if at least one table we delete from is transactional */
@@ -3109,6 +3108,7 @@ public:
   bool send_eof();
   virtual void abort();
   TABLE_LIST* get_tables() {return delete_tables;} /*@Bug 6121 to get the table to be delete from */
+  uint num_of_tables;
 };
 
 
