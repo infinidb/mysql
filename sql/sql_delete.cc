7d464338d98cebce7a802ed88cd28eb953e5a019
Bob Dempsey <rdempsey@infinidb.co>
2014-07-17 11:05:02 -0500

    fix init order

--- sql/sql_delete.cc
+++ sql/sql_delete.cc
@@ -660,8 +660,9 @@ int mysql_multi_delete_prepare(THD *thd, uint *table_count)
 
 multi_delete::multi_delete(TABLE_LIST *dt, uint num_of_tables_arg)
   : delete_tables(dt), deleted(0), found(0),
-    num_of_tables(num_of_tables_arg), error(0),
-    do_delete(0), transactional_tables(0), normal_tables(0), error_handled(0)
+    error(0),
+    do_delete(0), transactional_tables(0), normal_tables(0), error_handled(0),
+    num_of_tables(num_of_tables_arg)
 {
   tempfiles= (Unique **) sql_calloc(sizeof(Unique *) * num_of_tables);
 }
